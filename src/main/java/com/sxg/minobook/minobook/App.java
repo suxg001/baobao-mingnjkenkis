3b4efd9990c0561bbe2db35ecbc614af8a88377a
suxiaoguang <suxiaoguang@qianjins.com>
2016-09-12 13:45:19 +0800

    first commit

---
+++ src/main/java/com/sxg/minobook/minobook/App.java
@@ -0,0 +1,13 @@
+package com.sxg.minobook.minobook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
